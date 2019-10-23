a09651f0a17f01d29d9fc69d67c443c517242775
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 12:04:53 -0500

    added reflec-metada to server and snake naming strategy

--- src/server.ts
+++ src/server.ts
@@ -3,24 +3,38 @@ import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import errorHandlers from "./middleware/errorHandlers";
 import routes from "./routes";
+import "reflect-metadata";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-process.on("uncaughtException", e => {
-  console.log(e);
-  process.exit(1);
-});
+getConnectionOptions()
+  .then(connectionOptions => {
+    return createConnection({
+      ...connectionOptions,
+      namingStrategy: new SnakeNamingStrategy()
+    }).then(async connection => {
+      process.on("uncaughtException", e => {
+        console.log(e);
+        process.exit(1);
+      });
 
-process.on("unhandledRejection", e => {
-  console.log(e);
-  process.exit(1);
-});
+      process.on("unhandledRejection", e => {
+        console.log(e);
+        process.exit(1);
+      });
 
-const router = express();
-applyMiddleware(middleware, router);
-applyRoutes(routes, router);
-applyMiddleware(errorHandlers, router);
+      const router = express();
+      applyMiddleware(middleware, router);
+      applyRoutes(routes, router);
+      applyMiddleware(errorHandlers, router);
 
-const { PORT = 3000 } = process.env;
+      const { PORT = 3000 } = process.env;
 
-router.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}...`);
-});
+      router.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
