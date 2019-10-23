7d02ff2a6fd0feac39e0a4a3dc640a8fc22895c3
Richard Muñoz <richard.munoz@crifa.com>
2019-10-23 17:32:51 -0500

    deleted provider

--- src/http/providers/jsonPlaceholderProvider.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getUsers = async () => {
-  const url = process.env.JSON_PLACEHOLDER_ULR;
-  const response = await axios.get(`${url}/users`);
-  return response.data;
-};
