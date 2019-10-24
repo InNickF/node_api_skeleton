e78475ca9ae1db4010bc0c51f498e10a31c7efd2
Richard Muñoz <richard.munoz@crifa.com>
2019-10-24 17:21:55 -0500

    added custom validators for auth routes

--- src/http/validators/authValidator.ts
+++ src/http/validators/authValidator.ts
@@ -1,20 +1,63 @@
 import Validator from "../../utils/validator";
 import { Request, Response, NextFunction } from "express";
 import transformValidationErrors from "../transformers/errorTransformer";
+import "./customValidators";
 
 export const validateRegister = (req: Request, res: Response, next: NextFunction) => {
   const data = req.body;
   const rules = {
-    username: "required|string|min:4|max:50",
-    password: "required|string|min:8|max:20",
+    username: "required|string|min:4|max:20|username_available",
+    password: "required|string|min:4|max:20",
     role: ["required", { in: ["ADMIN", "USER"] }]
   };
 
   const validator = new Validator(data, rules);
-  if (validator.fails()) {
+  const passes = () => {
+    next();
+  };
+  const fails = () => {
     const formatedErrors = transformValidationErrors(validator.errors);
     res.status(422).send(formatedErrors);
-  } else {
+  };
+
+  validator.checkAsync(passes, fails);
+};
+
+export const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+  const data = req.body;
+  const rules = {
+    username: "required|string|min:4|max:50|username_exists",
+    password: "required|string|min:4|max:20"
+  };
+
+  const validator = new Validator(data, rules);
+  const passes = () => {
     next();
-  }
+  };
+  const fails = () => {
+    const formatedErrors = transformValidationErrors(validator.errors);
+    res.status(422).send(formatedErrors);
+  };
+
+  validator.checkAsync(passes, fails);
+};
+
+export const validateChangePassword = (req: Request, res: Response, next: NextFunction) => {
+
+  const data = req.body;
+  const rules = {
+    password: "required|string|min:4|max:20",
+    newPassword: "required|string|min:4|max:20"
+  };
+
+  const validator = new Validator(data, rules);
+  const passes = () => {
+    next();
+  };
+  const fails = () => {
+    const formatedErrors = transformValidationErrors(validator.errors);
+    res.status(422).send(formatedErrors);
+  };
+
+  validator.checkAsync(passes, fails);
 };
---
+++ src/http/validators/customValidators.ts
@@ -0,0 +1,30 @@
+import Validator from "../../utils/validator";
+import { findByUsername } from "../repositories/userRepository";
+
+/* ****************************************************************
+ * You can register new validators with the following functions:  *
+ * Validator.register(name, callbackFn, errorMessage);            *
+ * Validator.registerAsync(name, callbackFn, errorMessage);       *
+ ******************************************************************/
+
+Validator.registerAsync(
+  "username_available",
+  async (username: string, attribute, req, passes) => {
+    if (await findByUsername(username)) {
+      passes(false);
+    }
+    passes();
+  },
+  "Username is already in use."
+);
+
+Validator.registerAsync(
+  "username_exists",
+  async (username: string, attribute, req, passes) => {
+    if (await findByUsername(username)) {
+      passes();
+    }
+    passes(false);
+  },
+  "Username is incorrect"
+);
