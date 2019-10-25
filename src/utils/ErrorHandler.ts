e774e4bae5fc18bd05c8e5a1e4b9c7bd1dd9488d
Richard Muñoz <richard.munoz@crifa.com>
2019-10-25 12:13:45 -0500

    fixed error handlers

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -5,26 +5,31 @@ export const notFoundError = () => {
   throw new HTTP404Error("Method not found.");
 };
 
-// TODO: finish error handlers
-
 export const unauthorizedError = () => {
   throw new HTTP401Error("Unauthorized.");
 };
 
 export const clientError = (err: Error, res: Response, next: NextFunction) => {
   if (err instanceof HTTPClientError) {
-    console.warn(err);
-    res.status(err.statusCode).send({ status: "fail", message: err.message });
+    if (process.env.NODE_ENV !== "production") {
+      console.warn(err);
+    }
+    // set err.data for errors coming from services
+    res.status(err.statusCode).send({
+      status: "fail",
+      error: err.name,
+      message: err.message,
+      data: err.data
+    });
   } else {
     next(err);
   }
 };
 
 export const serverError = (err: Error, res: Response, next: NextFunction) => {
-  console.error(err);
   if (process.env.NODE_ENV === "production") {
     res.status(500).send("Internal Server Error");
-  } else {
-    res.status(500).send(err.stack);
   }
+  console.error(err);
+  res.status(500).send(err.stack);
 };
