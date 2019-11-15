cd7f13bba2ebca67b826e1487747641f0c4c4574
Richard Muñoz <richard.munoz@crifa.com>
2019-11-15 11:26:20 -0500

    added dynamic roles to routes

--- src/http/routes/settingsRoutes.ts
+++ src/http/routes/settingsRoutes.ts
@@ -1,19 +1,24 @@
 import * as settingsController from "../controllers/settingsController";
 import { checkJwt } from "../middleware/jwt";
 import { checkRole } from "../middleware/role";
+import { userRole } from "../entities/User";
 
 export default [
   {
     path: "/api/v1/settings",
     method: "get",
-    handler: [checkJwt, checkRole(["DEVELOPER"]), settingsController.getAll]
+    handler: [
+      checkJwt,
+      checkRole([userRole.DEVELOPER]),
+      settingsController.getAll
+    ]
   },
   {
-    path: "/api/v1/settings/update",
+    path: "/api/v1/settings",
     method: "post",
     handler: [
       checkJwt,
-      checkRole(["DEVELOPER"]),
+      checkRole([userRole.DEVELOPER]),
       settingsController.updateSettings
     ]
   }
--- src/http/routes/usersRoutes.ts
+++ src/http/routes/usersRoutes.ts
@@ -1,11 +1,12 @@
 import * as usersController from "../controllers/usersController";
 import { checkJwt } from "../middleware/jwt";
 import { checkRole } from "../middleware/role";
+import {userRole} from "../entities/User";
 
 export default [
   {
     path: "/api/v1/users",
     method: "get",
-    handler: [checkJwt, checkRole(["USER"]), usersController.getUsers]
+    handler: [checkJwt, checkRole([userRole.USER]), usersController.getUsers]
   }
 ];
