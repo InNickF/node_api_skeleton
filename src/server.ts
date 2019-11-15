fb73cb870d27ac09826adf698c6ab66c207df9d7
Richard Muñoz <richard.munoz@crifa.com>
2019-11-15 11:54:52 -0500

    added logger to server

--- src/server.ts
+++ src/server.ts
@@ -32,10 +32,16 @@ getConnectionOptions()
       const { PORT = 3000 } = process.env;
 
       router.listen(PORT, () => {
+        if (process.env.NODE_ENV === "production") {
+          Logger.info.info(`Server is running on http://localhost:${PORT}`);
+        }
         console.log(`Server is running on http://localhost:${PORT}`);
       });
     });
   })
   .catch(error => {
-    Logger.err.error(error);
+    if (process.env.NODE_ENV === "production") {
+      Logger.err.error(error.message);
+    }
+    console.log(error.message);
   });
